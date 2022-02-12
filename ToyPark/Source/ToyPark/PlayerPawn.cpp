5999d6bfbb0c04e37991d416be71ba6db69dfc61
hassan khzazaal <hassan.khazaal.drive@gmail.com>
2022-02-12 13:34:18 +0200

    Add Player Movement

--- ToyPark/Source/ToyPark/PlayerPawn.cpp
+++ ToyPark/Source/ToyPark/PlayerPawn.cpp
@@ -1,19 +1,54 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PlayerPawn.h"
+
+#include <ThirdParty/openexr/Deploy/OpenEXR-2.3.0/OpenEXR/include/ImathMath.h>
 
 // Sets default values
 APlayerPawn::APlayerPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-	RootComponent = Camera;
 	
 	bat = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bat"));
 	bat->SetupAttachment(Camera);
+}
+
+void APlayerPawn::MoveForward(float value)
+{
+	FVector offset =  Camera->GetForwardVector() * (value * MoveSpeed);
+	AddActorWorldOffset(offset);
+}
+
+void APlayerPawn::MoveRight(float value)
+{
+	FVector offset =  Camera->GetRightVector() * (value * MoveSpeed);
+	AddActorWorldOffset(offset);
+}
+
+void APlayerPawn::MoveUp(float value)
+{
+	FVector offset =  Camera->GetUpVector() * (value * MoveSpeed);
+	AddActorWorldOffset(offset);
+}
+
+void APlayerPawn::LookUp(float value)
+{
+	if(bCanLook)
+		Camera->AddRelativeRotation(FRotator(value * LookSensitivity, 0, 0));
+}
+
+void APlayerPawn::LookRight(float value)
+{
+	if(bCanLook)
+		Camera->AddRelativeRotation(FRotator(0, value * LookSensitivity, 0));
+}
+
+void APlayerPawn::ToggleLook(bool canLook)
+{
+	bCanLook = canLook;
 }
 
 // Called when the game starts or when spawned
@@ -35,5 +70,14 @@ void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	DECLARE_DELEGATE_OneParam(FCustomInputDelegate, const bool);
+	PlayerInputComponent->BindAction<FCustomInputDelegate>("LookAround", IE_Pressed, this, &APlayerPawn::ToggleLook, true);
+	PlayerInputComponent->BindAction<FCustomInputDelegate>("LookAround", IE_Released, this, &APlayerPawn::ToggleLook, false);
+
+	PlayerInputComponent->BindAxis("LookUp", this, &APlayerPawn::LookUp);
+	PlayerInputComponent->BindAxis("LookRight", this, &APlayerPawn::LookRight);
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerPawn::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerPawn::MoveRight);
+	PlayerInputComponent->BindAxis("MoveUp", this, &APlayerPawn::MoveUp);
 }
 
--- ToyPark/Source/ToyPark/PlayerPawn.h
+++ ToyPark/Source/ToyPark/PlayerPawn.h
@@ -7,6 +7,7 @@
 #include "GameFramework/Pawn.h"
 #include "PlayerPawn.generated.h"
 
+class SceneComponent;
 class StaticMeshComponent;
 class CameraComponent;
 
@@ -16,22 +17,45 @@ class TOYPARK_API APlayerPawn : public APawn
 	GENERATED_BODY()
 
 private:
-	UPROPERTY(EditAnywhere, Category = "Components")
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UStaticMeshComponent* bat;
 
-	UPROPERTY(EditAnywhere, Category = "Components")
+	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UCameraComponent* Camera;
+
+	UPROPERTY(VisibleAnywhere)
+	bool bCanLook;
+
+	UFUNCTION()
+	void ToggleLook(bool canLook);
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:
 	APlayerPawn();
-
+	
+	UFUNCTION()
+	void MoveForward(float value);	
+	UFUNCTION()
+	void MoveRight(float value);
+	UFUNCTION()
+	void MoveUp(float value);
+	UFUNCTION()
+	void LookUp(float value);
+	UFUNCTION()
+	void LookRight(float value);
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	float MoveSpeed;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	float LookSensitivity;
 };
